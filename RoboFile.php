f6eff59d34d9fbfd64c6aa7697c7cf60b3377873
Łukasz Biały <lukasz@keios.eu>
2015-08-27 20:47:21 +0200

    download composer.phar locally, change for travis-ci

--- RoboFile.php
+++ RoboFile.php
@@ -15,7 +15,37 @@ class RoboFile extends \Robo\Tasks
 
     public function prepare()
     {
-        $this->taskComposerInstall()
+        $this->stopOnFail(true);
+
+        $buildDir = __DIR__.'/build';
+        $composerPharPath = __DIR__.'/build/composer.phar';
+
+        if (!file_exists($buildDir)) {
+            $this->say('Creating build directory...');
+            mkdir($buildDir, 0755);
+        }
+
+        if (!file_exists($composerPharPath)) {
+            $handle = fopen($composerPharPath, 'w');
+            $this->say('Downloading composer.phar...');
+            $options = [
+                CURLOPT_RETURNTRANSFER => 1,
+                CURLOPT_FILE           => $handle,
+                CURLOPT_TIMEOUT        => 360,
+                CURLOPT_URL            => 'https://getcomposer.org/composer.phar'
+            ];
+
+            $ch = curl_init();
+            curl_setopt_array($ch, $options);
+            curl_exec($ch);
+            curl_close($ch);
+
+            fclose($handle);
+            
+            chmod($composerPharPath, 0755);
+        }
+
+        $this->taskComposerInstall($composerPharPath)
             ->optimizeAutoloader()
             ->run();
 
@@ -33,7 +63,7 @@ class RoboFile extends \Robo\Tasks
 
     public function behat()
     {
-	$this->stopOnFail(true);	
+        $this->stopOnFail(true);
 
         $this->taskExec('./node_modules/.bin/phantomjs --webdriver=4444 --webdriver-loglevel=WARNING')
             ->background()
@@ -43,7 +73,7 @@ class RoboFile extends \Robo\Tasks
             ->background()
             ->run();
 
-        $microSecondsToWait = (1/10) * 1000000; // 1/10 of second (100ms)
+        $microSecondsToWait = (1 / 10) * 1000000; // 1/10 of second (100ms)
         $this->say(sprintf('Waiting for %dms for phantomjs and php server to start.', $microSecondsToWait / 1000));
         usleep($microSecondsToWait);
 
