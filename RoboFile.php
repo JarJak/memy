8fdfcb15ad3620bece87183c0155d31337d2688d
Łukasz Biały <lukasz@keios.eu>
2015-08-28 22:57:58 +0200

    refactor robofile to test running only

--- RoboFile.php
+++ RoboFile.php
@@ -4,56 +4,6 @@
 
 class RoboFile extends \Robo\Tasks
 {
-
-    public $basedir = __DIR__;
-
-    public function build()
-    {
-        $this->prepare();
-        $this->behat();
-    }
-
-    public function prepare()
-    {
-        $this->stopOnFail(true);
-
-        $buildDir = __DIR__.'/build';
-        $composerPharPath = __DIR__.'/build/composer.phar';
-
-        if (!file_exists($buildDir)) {
-            $this->say('Creating build directory...');
-            mkdir($buildDir, 0755);
-        }
-
-        if (!file_exists($composerPharPath)) {
-            $handle = fopen($composerPharPath, 'w');
-            $this->say('Downloading composer.phar...');
-            $options = [
-                CURLOPT_RETURNTRANSFER => 1,
-                CURLOPT_FILE           => $handle,
-                CURLOPT_TIMEOUT        => 360,
-                CURLOPT_URL            => 'https://getcomposer.org/composer.phar'
-            ];
-
-            $ch = curl_init();
-            curl_setopt_array($ch, $options);
-            curl_exec($ch);
-            curl_close($ch);
-
-            fclose($handle);
-
-            chmod($composerPharPath, 0755);
-        }
-
-        $this->taskComposerInstall($composerPharPath)
-            ->optimizeAutoloader()
-            ->run();
-
-        $this->npm();
-
-        $this->taskExec('php app/console assets:install --symlink')->run();
-    }
-
     public function npm()
     {
         $this->taskExec('npm')->arg('install')->run();
@@ -62,6 +12,8 @@ class RoboFile extends \Robo\Tasks
     public function behat()
     {
         $this->stopOnFail(true);
+
+        $this->npm();
 
         $this->taskExec('./node_modules/.bin/phantomjs --webdriver=4444 --webdriver-loglevel=ALL')
             ->background()
@@ -75,10 +27,13 @@ class RoboFile extends \Robo\Tasks
         $this->say(sprintf('Waiting for %dms for phantomjs and php server to start.', $microSecondsToWait / 1000));
         usleep($microSecondsToWait);
 
-        $this->taskExec('curl http://localhost:8000/app_test.php')
+        $this->taskExec('./bin/behat')
             ->run();
 
-        $this->taskExec('./bin/behat')
+        $this->taskExec('killall node')
+            ->run();
+
+        $this->taskExec('killall php')
             ->run();
     }
 }
